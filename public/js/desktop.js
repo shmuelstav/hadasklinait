3845e243ffe81f2eb989c7c916f1274f57da6237
shmuelstav <shm_had@walla.co.il>
2017-10-09 21:57:56 +0300

    remove mobile

--- public/js/desktop.js
+++ public/js/desktop.js
@@ -4,7 +4,7 @@
 $(document).ready(function() {
 
     /*google analitics */
-
+/*
     (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
             (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
         m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -13,7 +13,7 @@ $(document).ready(function() {
     ga('create', 'UA-61270998-3', 'auto');
     ga('send', 'pageview');
 
-
+*/
 
     function goToByScroll(id){
      // Reove "link" from the ID
